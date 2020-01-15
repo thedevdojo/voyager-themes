1428dcd2aa32a70c169d5eb18386ab92a83304bf
Henry <henry@heroleads.com>
2020-01-15 18:52:52 +0700

    Fix bug about cannot call getKey() method

--- src/helpers.php
+++ src/helpers.php
@@ -1,29 +1,26 @@
 <?php
 
 
-if (!function_exists(theme_field)){
+if (!function_exists('theme_field')) {
 
-	function theme_field($type, $key, $title, $content = '', $details = '', $placeholder = '', $required = 0){
+    function theme_field($type, $key, $title, $content = '', $details = '', $placeholder = '', $required = 0){
 
-		$theme = \VoyagerThemes\Models\Theme::where('folder', '=', ACTIVE_THEME_FOLDER)->first();
+        $theme = \VoyagerThemes\Models\Theme::where('folder', '=', ACTIVE_THEME_FOLDER)->first();
 
-		$option_exists = $theme->options->where('key', '=', $key)->first();
+        $option_exists = $theme->options->where('key', '=', $key)->first();
 
-		if ($option_exists) {
-		    $id = $option_exists->id;
-			$content = $option_exists->value;
-		} else {
-		    $id = "";
-		    $content = "";
+        if (!$option_exists) {
+            return "";
         }
 
-		$row = (object)['required' => $required, 'field' => $key, 'type' => $type, 'details' => $details, 'display_name' => $placeholder];
-		$dataTypeContent = (object)['id' => $id, $key => $content];
-		$label = '<label for="'. $key . '">' . $title . '<span class="how_to">You can reference this value with <code>theme(\'' . $key . '\')</code></span></label>';
-		$details = '<input type="hidden" value="' . $details . '" name="' . $key . '_details__theme_field">';
-		$type = '<input type="hidden" value="' . $type . '" name="' . $key . '_type__theme_field">';
-		return $label . app('voyager')->formField($row, '', $dataTypeContent) . $details . $type . '<hr>';
-	}
+        $option_exists->{$key} = $option_exists->value;
+
+        $row = (object)['required' => $required, 'field' => $key, 'type' => $type, 'details' => $details, 'display_name' => $placeholder];
+        $label = '<label for="'. $key . '">' . $title . '<span class="how_to">You can reference this value with <code>theme(\'' . $key . '\')</code></span></label>';
+        $details = '<input type="hidden" value="' . $details . '" name="' . $key . '_details__theme_field">';
+        $type = '<input type="hidden" value="' . $type . '" name="' . $key . '_type__theme_field">';
+        return $label . app('voyager')->formField($row, '', $option_exists) . $details . $type . '<hr>';
+    }
 
 }
 
